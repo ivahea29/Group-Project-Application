562c3ee5096a93e901e4cb0fcdc9fd70a35ae18d
ivahea29 <102086981+ivahea29@users.noreply.github.com>
2023-05-09 19:56:34 +1200

    Changed gradle dependencies for Firebase

--- app/src/main/java/com/example/groupprojectapplication/MainActivity.kt
+++ app/src/main/java/com/example/groupprojectapplication/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.groupprojectapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseApp.initializeApp(this);
+
     }
 }
